5c725f2c20eef75ed7a7ae0f1aa8927f2f54e9c8
Mrowr Purr <mrowr.purr@gmail.com>
2022-11-11 20:13:11 -0800

    Print() output includes name of script

--- include/SkyrimScripting/Spec/Implementation/PapyrusInterface.h
+++ include/SkyrimScripting/Spec/Implementation/PapyrusInterface.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#undef GetObject
+
 namespace SkyrimScripting::Spec::Implementation::PapyrusInterface {
 
-    // TODO - use VMStackID to get the name of the test the output is coming from!
-    void Specification_Print(RE::BSScript::Internal::VirtualMachine*, const RE::VMStackID, RE::Actor*, std::string text) {
-        TestOutput::GetSingleton().Output(text);
+    void Specification_Print(RE::BSScript::Internal::VirtualMachine*, const RE::VMStackID stackID, RE::Actor*, std::string text) {
+        auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+        auto stack = vm->allRunningStacks.find(stackID);
+        auto scriptName = stack->second->top->previousFrame->self.GetObject()->GetTypeInfo()->GetName();
+        TestOutput::GetSingleton().Output(std::format("[{}] {}", scriptName, text));
     }
 
     bool BIND(RE::BSScript::IVirtualMachine* vm) {
