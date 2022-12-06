28e4b984c134a5f2ecee004bf0401cff57e2d896
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-05 22:11:52 -0800

    [GoogleTest] Now make a test which required the game to be started and make it work Fixes #18
    Includes option to run a system script at the Main Menu: SPEC_GAME_START_SCRIPT
    
    This can be defined either via a #define SPEC_GAME_START_SCRIPT macro or by setting SPEC_GAME_START_SCRIPT variable.
    
    An example .ahk AutoHotkey script is provided which will `coc riverwood to start the game!

--- Examples/GoogleTest/tests.cpp
+++ Examples/GoogleTest/tests.cpp
@@ -4,26 +4,66 @@
 #define SPEC_RUN_IMMEDIATE
 // #define SPEC_RUN_PLUGINS_LOADED
 #define SPEC_RUN_MODS_LOADED
+#define SPEC_RUN_GAME_STARTED
+#define SPEC_EXIT_AFTER_RUN
+
+// Specify via #define or set the SPEC_GAME_START_SCRIPT environment variable
+// #define SPEC_GAME_START_SCRIPT R"(C:\Code\Skyrim Scripting Projects\Spec\coc)"
 
 #include <gtest/gtest.h>
 
 TEST(SpecRunImmediate_TestSuite, CanGetPluginName) {
+	// Getting the PluginDeclaration only works when the game is running
+	// but it doens't have any other dependencies
 	auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
 	EXPECT_EQ(pluginName, "SkyrimScripting.Spec.Example.GoogleTest");
 }
 
 TEST(SpecRunModsLoaded_TestSuite, CanGetTheNameOfAQuest) {
+	// Querying for Forms breaks unless mods data has been loaded (kDataLoaded)
 	auto* mainQuest = RE::TESForm::LookupByEditorID("MQ101");
 	EXPECT_STREQ(mainQuest->GetName(), "Unbound");
 }
 
-void RunGoogleTests(const std::string& filter) {
-	std::cout << std::format("[RunGoogleTests] filter:'{}'", filter) << std::endl;
-	std::vector<std::string> args = {"", std::format("--gtest_filter=*{}*", filter)};
-	std::vector<char*> argv;
-	for (const auto& arg : args) argv.push_back((char*)arg.data());
-	int argc = (int)argv.size();
-	::testing::InitGoogleTest(&argc, argv.data());
+TEST(SpecRunModsLoaded_TestSuite, CanGetReturnTypeOfPapyrusFunction) {
+	// Interacting with the VM... hmm... let's find out?
+	// Actually, this works without the game running because it's only introspection.
+	auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+	ASSERT_TRUE(vm != nullptr);
+
+	RE::BSTSmartPointer<RE::BSScript::ObjectTypeInfo> typeInfo;
+	vm->GetScriptObjectType("Actor", typeInfo);
+	ASSERT_TRUE(typeInfo.get() != nullptr);
+
+	auto* functions = typeInfo.get()->GetMemberFuncIter();
+	auto functionCount = typeInfo.get()->GetNumMemberFuncs();
+	auto functionName = RE::BSFixedString("GetCrimeFaction");
+	RE::BSTSmartPointer<RE::BSScript::IFunction> Actor_getCrimeFaction;
+	for (uint32_t i = 0; i < functionCount; i++) {
+		if (functions[i].func->GetName() == functionName) {
+			Actor_getCrimeFaction = functions[i].func;
+			break;
+		}
+	}
+	ASSERT_TRUE(Actor_getCrimeFaction.get() != nullptr);
+
+	// Actor.GetCrimeFaction returns a Faction
+	ASSERT_STREQ(Actor_getCrimeFaction.get()->GetReturnType().GetTypeInfo()->GetName(), "Faction");
+}
+
+TEST(SpecRunGameStart_TestSuite, CanGetPlayersCurrentLocation) {
+	// Get the player's current location? Hmm, it probably has a default?
+	auto* player = RE::TESForm::LookupByID(0x14)->As<RE::TESObjectREFR>();
+	EXPECT_TRUE(player != nullptr);
+
+	auto location = player->GetCurrentLocation();
+	EXPECT_TRUE(location != nullptr);
+	EXPECT_STREQ(location->GetFullName(), "Riverwood");
+}
+
+void RunGoogleTests(std::string filter) {
+	GTEST_FLAG_SET(filter, std::format("*{}*", filter));
+	::testing::InitGoogleTest();
 	RUN_ALL_TESTS();
 }
 
@@ -44,11 +84,40 @@ std::string GetTestFilterForEvent(TestRunEvent event) {
 	}
 }
 
+class CellFullyLoadedEventSink : public RE::BSTEventSink<RE::TESCellFullyLoadedEvent> {
+	CellFullyLoadedEventSink() = default;
+	~CellFullyLoadedEventSink() = default;
+	CellFullyLoadedEventSink(const CellFullyLoadedEventSink&) = delete;
+	CellFullyLoadedEventSink(CellFullyLoadedEventSink&&) = delete;
+	CellFullyLoadedEventSink& operator=(const CellFullyLoadedEventSink&) = delete;
+	CellFullyLoadedEventSink& operator=(CellFullyLoadedEventSink&&) = delete;
+	std::atomic<bool> loaded = false;
+
+public:
+	static CellFullyLoadedEventSink& GetSingleton() {
+		static CellFullyLoadedEventSink singleton;
+		return singleton;
+	}
+
+	RE::BSEventNotifyControl ProcessEvent(const RE::TESCellFullyLoadedEvent*,
+										  RE::BSTEventSource<RE::TESCellFullyLoadedEvent>*) override {
+		if (!loaded.exchange(true)) {
+			RunGoogleTests(GetTestFilterForEvent(TestRunEvent::GameStarted));
+#if defined(SPEC_EXIT_AFTER_RUN)
+			SKSE::WinAPI::TerminateProcess(SKSE::WinAPI::GetCurrentProcess(), EXIT_SUCCESS);
+#endif
+		}
+		return RE::BSEventNotifyControl::kContinue;
+	}
+};
+
 void RedirectStdoutToFile() {
 	auto stdout_path = SKSE::log::log_directory();
 	*stdout_path /= fmt::format("{}.test.log", SKSE::PluginDeclaration::GetSingleton()->GetName());
 	freopen(stdout_path->string().c_str(), "w", stdout);
 }
+
+#include "Windows.h"
 
 SKSEPluginLoad(const SKSE::LoadInterface* skse) {
 	RedirectStdoutToFile();
@@ -60,6 +129,10 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse) {
 #if defined(SPEC_RUN_IMMEDIATE)
 	std::cout << "Running SpecRunImmediate tests" << std::endl;
 	RunGoogleTests(GetTestFilterForEvent(TestRunEvent::Immediate));
+#if defined(SPEC_EXIT_AFTER_RUN) && !defined(SPEC_RUN_PLUGINS_LOADED) && !defined(SPEC_RUN_MODS_LOADED) && \
+	!defined(SPEC_RUN_GAME_STARTED)
+	SKSE::WinAPI::TerminateProcess(SKSE::WinAPI::GetCurrentProcess(), EXIT_SUCCESS);
+#endif
 #endif
 #if defined(SPEC_RUN_PLUGINS_LOADED) || defined(SPEC_RUN_MODS_LOADED)
 	SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message) {
@@ -67,15 +140,39 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse) {
 		if (message->type == SKSE::MessagingInterface::kPostLoad) {
 			std::cout << "Running SpecRunPluginsLoaded tests" << std::endl;
 			RunGoogleTests(GetTestFilterForEvent(TestRunEvent::PluginsLoaded));
+#if defined(SPEC_EXIT_AFTER_RUN) && !defined(SPEC_RUN_MODS_LOADED) && !defined(SPEC_RUN_GAME_STARTED)
+			SKSE::WinAPI::TerminateProcess(SKSE::WinAPI::GetCurrentProcess(), EXIT_SUCCESS);
+#endif
 		}
 #endif
 #if defined(SPEC_RUN_MODS_LOADED)
-		if (message->type == SKSE::MessagingInterface::kPostLoad) {
-			std::cout << "Running SpecRunModsLoaded tests" << std::endl;
+		if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+			std::cout << "Runnings SpecRunModsLoaded tests" << std::endl;
 			RunGoogleTests(GetTestFilterForEvent(TestRunEvent::ModsLoaded));
+#if defined(SPEC_EXIT_AFTER_RUN) && !defined(SPEC_RUN_GAME_STARTED)
+			SKSE::WinAPI::TerminateProcess(SKSE::WinAPI::GetCurrentProcess(), EXIT_SUCCESS);
+#endif
+#if defined(SPEC_RUN_GAME_STARTED)
+#if defined(SPEC_GAME_START_SCRIPT)
+			std::cout << std::format("Running game start script: '{}'", SPEC_GAME_START_SCRIPT) << std::endl;
+			WinExec(SPEC_GAME_START_SCRIPT, SW_HIDE);
+#else
+			auto* scriptPath = std::getenv("SPEC_GAME_START_SCRIPT");
+			if (scriptPath) {
+				std::cout << std::format("Running game start script: '{}'", scriptPath) << std::endl;
+				WinExec(scriptPath, SW_HIDE);
+			} else {
+				std::cout << "No game start script found" << std::endl;
+			}
+#endif
+#endif
 		}
 #endif
 	});
+#if defined(SPEC_RUN_GAME_STARTED)
+	auto eventSource = RE::ScriptEventSourceHolder::GetSingleton()->GetEventSource<RE::TESCellFullyLoadedEvent>();
+	eventSource->AddEventSink(&CellFullyLoadedEventSink::GetSingleton());
+#endif
 #endif
 #else
 	std::cout << "No tests defined." << std::endl;
