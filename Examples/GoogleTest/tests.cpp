5dade909f7054ba3a2afaa34c4a9a4fcbcb5dfa9
Mrowr Purr <87039589+mrowrpurr@users.noreply.github.com>
2022-12-05 17:46:00 -0800

    [GoogleTest] Get test working at runtime (which doesn't work if not running in the game) (#17)

--- Examples/GoogleTest/tests.cpp
+++ Examples/GoogleTest/tests.cpp
@@ -4,45 +4,34 @@
 #include <gtest/gtest.h>
 
 TEST(FirstTestSuite, HelloWorld) {
-	// auto* whatever = RE::TESForm::LookupByEditorID("Hod");
-	// EXPECT_EQ(whatever->GetFormID(), 123);
+	auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
+	EXPECT_EQ(pluginName, "SkyrimScripting.Spec.Example.GoogleTest");
 
-	// auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
-	// EXPECT_EQ(pluginName, "???");
-
-	auto messagingVersion = SKSE::GetMessagingInterface()->Version();
-	EXPECT_EQ(messagingVersion, 123);
+	// If not kDataLoaded, this gets:
+	// unknown file: error: SEH exception with code 0xc0000005 thrown in the test body.
+	// auto* mainQuest = RE::TESForm::LookupByEditorID("MQ101");
+	// EXPECT_STREQ(mainQuest->GetName(), "Unbound");
 }
 TEST(SecondTestSuite, GoodnightMoon) { EXPECT_EQ(111, 222); }
 
-// Providing your own main() function with GoogleTest
-// http://google.github.io/googletest/primer.html#writing-the-main-function
-int main(int _argc, char** _argv) {
-	// Running a subset of the tests with GoogleTest
-	// https://github.com/google/googletest/blob/main/docs/advanced.md#running-a-subset-of-the-tests
-	std::vector<std::string> args = {"", "--gtest_filter=*First*"};
-
-	// Generate `argc` and `argv`, combining inputs with `args` specified above.
-	std::vector<char*> argv{_argv + 1, _argv + _argc};
-	for (const auto& arg : args) argv.push_back((char*)arg.data());
-	int argc = (int)argv.size();
-
-	::testing::InitGoogleTest(&argc, argv.data());
-	return RUN_ALL_TESTS();
+void RedirectStdoutToFile() {
+	auto stdout_path = SKSE::log::log_directory();
+	*stdout_path /= fmt::format("{}.test.log", SKSE::PluginDeclaration::GetSingleton()->GetName());
+	freopen(stdout_path->string().c_str(), "w", stdout);
 }
 
-// std::ofstream loggerOutputForStdoutRedirection;
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+	RedirectStdoutToFile();
+	SKSE::Init(skse);
+	std::cout << "Running tests..." << std::endl;
 
-// void RedirectStdoutToFile() {
-// 	auto stdout_path = SKSE::log::log_directory();
-// 	*stdout_path /= fmt::format("{}.test.log", SKSE::PluginDeclaration::GetSingleton()->GetName());
-// 	loggerOutputForStdoutRedirection = std::ofstream{stdout_path->string()};
-// 	std::cout.rdbuf(loggerOutputForStdoutRedirection.rdbuf());
-// }
+	// Run GoogleTest
+	std::vector<std::string> args = {"", "--gtest_filter=*First*"};
+	std::vector<char*> argv;
+	for (const auto& arg : args) argv.push_back((char*)arg.data());
+	int argc = (int)argv.size();
+	::testing::InitGoogleTest(&argc, argv.data());
+	RUN_ALL_TESTS();
 
-// SKSEPluginLoad(const SKSE::LoadInterface* skse) {
-// 	RedirectStdoutToFile();
-// 	SKSE::Init(skse);
-// 	std::cout << "Initializing test log" << std::endl;
-// 	return true;
-// }
+	return true;
+}
