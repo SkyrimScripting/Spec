7b8d7e13c3cd722639b03d65b22b8f2eb1e82962
Mrowr Purr <87039589+mrowrpurr@users.noreply.github.com>
2022-12-05 16:34:08 -0800

    Can run specific tests using filter (all frameworks) (#12)

--- Examples/Catch2/tests.cpp
+++ Examples/Catch2/tests.cpp
@@ -3,10 +3,21 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("My Test Suite", "Hello World") { REQUIRE(69 == 420); }
+TEST_CASE("First Test Suite", "Hello World") { REQUIRE(69 == 420); }
+TEST_CASE("Second Test Suite", "Goodnight Moon") { REQUIRE(111 == 222); }
 
 #include <catch2/catch_session.hpp>
 
 // Providing your own main() function with Catch2
 // https://github.com/catchorg/Catch2/blob/devel/docs/own-main.md#top
-int main(int argc, char* argv[]) { return Catch::Session().run(argc, argv); }
+int main(int _argc, char* _argv[]) {
+	// Specifying which tests to run
+	// https://github.com/catchorg/Catch2/blob/devel/docs/command-line.md#specifying-which-tests-to-run
+	std::vector<std::string> args = {"", "*First*"};
+
+	// Generate `argc` and `argv`, combining inputs with `args` specified above.
+	std::vector<char*> argv{_argv + 1, _argv + _argc};
+	for (const auto& arg : args) argv.push_back((char*)arg.data());
+
+	return Catch::Session().run((int)argv.size(), argv.data());
+}
--- Examples/doctest/tests.cpp
+++ Examples/doctest/tests.cpp
@@ -5,12 +5,16 @@
 
 #include <doctest/doctest.h>
 
-TEST_CASE("Hello World") { CHECK(69 == 420); }
+TEST_CASE("First: Hello World") { CHECK(69 == 420); }
+TEST_CASE("Second: Goodnight Moon") { CHECK(111 == 222); }
 
 // Providing your own main() function with doctest
 // https://github.com/doctest/doctest/blob/master/doc/markdown/main.md
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
+	// doctest command-line options
+	// https://github.com/doctest/doctest/blob/master/doc/markdown/commandline.md
 	doctest::Context context;
+	context.addFilter("test-case", "*First*");
 	context.applyCommandLine(argc, argv);
 	return context.run();
 }
