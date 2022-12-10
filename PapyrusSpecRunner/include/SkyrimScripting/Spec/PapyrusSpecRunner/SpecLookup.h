d82f641720708426ce2927c98ec76ae2d22d34ac
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-10 15:12:30 -0800

    Passing spec (find spec files)

--- PapyrusSpecRunner/include/SkyrimScripting/Spec/PapyrusSpecRunner/SpecLookup.h
+++ PapyrusSpecRunner/include/SkyrimScripting/Spec/PapyrusSpecRunner/SpecLookup.h
@@ -7,8 +7,6 @@ namespace SkyrimScripting::Spec::PapyrusSpecRunner {
 
     std::set<std::string> GetAllSpecScriptNames() {
         std::set<std::string> names;
-        for (auto& file : std::filesystem::directory_iterator("Data/Scripts"))
-            if (file.path().string().ends_with("_Spec.pex")) names.emplace(file.path().stem().string());
         return names;
     }
 }
